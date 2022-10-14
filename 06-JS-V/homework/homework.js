7addb329e8ea879faa2ba82f92cfa8560eb45732
rodri72 <70957624+rodri72@users.noreply.github.com>
2022-10-14 00:12:27 -0300

    tarea06

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -8,7 +8,32 @@ function crearUsuario() {
   // {{nombre}} debe ser el nombre definido en cada instancia
   // Devuelve la clase
   // Tu código:
+  
+    function Usuario (opciones) {
+      this.usuario = opciones.usuario;
+      this.nombre = opciones.nombre;
+      this.email = opciones.email;
+      this.password = opciones.password;
+    
+    } ;
+  
+
+     Usuario.prototype.saludar = function() {
+       return `Hola, mi nombre es ${this.nombre}`;
+
+     } ;
+  
+   return Usuario; 
+
+
 }
+
+  
+
+
+
+ 
+
 
 function agregarMetodoPrototype(Constructor) {
   // Agrega un método al Constructor del `prototype`
